4af1ad8ff6ebea8296e10e94f1564b41fa140499
Jehanzaib <jehanzaib.dev@gmail.com>
2026-06-03 19:56:57 +0500

    feat: move to profile page on topbar search click

--- src/components/topbar/topbar.jsx
+++ src/components/topbar/topbar.jsx
@@ -1,25 +1,51 @@
 
 import "./topbar.css";
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import { AuthContext } from "../../context/authContext.js";
 import { useNavigate } from "react-router-dom";
 import { Logout } from "../../context/authActions";
 import {Link} from 'react-router-dom';
-
+import {getAllUsersCall} from '../../apiCalls/apiCalls.js';
 
 export default function Topbar() {
 
   const PF=process.env.REACT_APP_PUBLIC_FOLDER;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [allUsers, setAllUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
   const { user, dispatch } = useContext(AuthContext);
   const navigate=useNavigate();
 
-  console.log(user);
+  const handleSearch = (e) => {
+   const value = e.target.value;
+   setSearchTerm(value);
+   const matches = allUsers.filter((user) =>
+      user.username
+         .toLowerCase()
+         .includes(value.toLowerCase())
+   );
+   setFilteredUsers(matches);
+};
+
+
   const handleLogout = () => {
     dispatch(Logout());
     navigate('/login');
   };
 
-  return (
+useEffect(() => {
+  const fetchUsers = async () => {
+    try {
+      const data =await getAllUsersCall(); 
+      setAllUsers(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  fetchUsers();
+}, []);
+
+return (
 
     <div className="topbarContainer">
 
@@ -40,8 +66,44 @@ export default function Topbar() {
           <input
             type="text"
             placeholder="Search friends, posts..."
-            className="searchInput"
+            className="searchInput" value={searchTerm} onChange={handleSearch}
           />
+          {
+          searchTerm && filteredUsers.length > 0 && (
+            <div className="searchResults">
+              {
+              filteredUsers.map((user) => (
+              <div
+                  key={user._id}
+                  className="searchUser"
+                  onClick={() => {
+
+   navigate(`/profile/${user?.username}`);
+
+   setSearchTerm("");
+
+   setFilteredUsers([]);
+
+}}
+               >
+  <img
+            src={user?.profilePic ? PF + user.profilePic : '/assets/person/noAvatar.jpeg'}
+            alt="profile"
+            className="searchImg"
+          />
+
+  <span>
+    {user?.username}
+  </span>   
+               </div>
+
+            ))
+         }
+
+      </div>
+
+   )
+}
 
         </div>
 
